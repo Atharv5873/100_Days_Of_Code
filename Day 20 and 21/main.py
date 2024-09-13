e5469e8fe3c8e12d08915d6c01d386f13780f5b4
Atharv <atharv5873@gmail.com>
2024-09-13 15:29:23 +0530

    "Added Food and ScoreBoard classes, instantiated objects, and implemented collision detection and score incrementation."

--- Day 20 and 21/main.py
+++ Day 20 and 21/main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle,Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -8,6 +10,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake=Snake()
+food=Food()
+scoreboard=ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -23,6 +27,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.08)
     snake.move()
+
+    if snake.head.distance(food) <15:
+        food.refresh()
+        scoreboard.increase_score()
     
     
      
