fef1973687175263a7d9adccac7cbc55c616975e
Atharv <atharv5873@gmail.com>
2024-09-14 22:01:39 +0530

    "Updated Food class in food.py, changed shape to turtle, added color options, and updated refresh method. Modified collision detection in main.py."

--- Day 20 and 21/food.py
+++ Day 20 and 21/food.py
@@ -1,15 +1,16 @@
 from turtle import Turtle
 import random
-
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape('circle')
+        self.shape('turtle')
         self.penup()
-        self.shapesize(0.5,0.5)
-        self.color('red')
+        self.shapesize(0.7,0.7)
+        self.color(random.choice(colors))
         self.speed('fastest')
         self.refresh()
 
     def refresh(self):
+        self.color(random.choice(colors))
         self.goto(random.randint(-280, 280), random.randint(-280, 280))
